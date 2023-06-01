5554b4428603f1a9153282088fbda9b492a086e9
Maciej Mogilewski <maciej.mogilewski@gmail.com>
2023-06-01 20:58:10 +0200

    destructuring

---
+++ es_features/destructuring.js
@@ -0,0 +1,38 @@
+const obj = {a: 1, b: 2, c: 3, d: 4};
+
+// const a = obj.a;
+// const b = obj.b;
+// const c = obj.c;
+// const d = obj.d;
+
+// destructuring - tworzenie zmiennych na podstawie elementów tablic lub obiketów
+
+// const {a, b, c, d} = obj;
+
+// const {a: aliasA, b, b = 42, d: aliasD = 4}
+// console.log(aliasA, b, c, aliasD)
+
+// const obj2 = {a: {b: 1, c: 2}, d: 42};
+
+// zrób zmienna b, c, d
+
+// const {a: {b, c}, d} = obj2;
+
+// console.log(b, c, d);
+
+
+// destrukturyzacja tablic
+
+const arr = [1, 2, 3, 4];
+
+// const [a, b, c, d] = arr;
+
+//const [a, b] = arr
+
+// const [a, b, ...rest] = arr;
+
+
+// dwa ostanie elemnty tablicy
+const [a, b] = arr.slice(-2)
+
+console.log(a, b)
